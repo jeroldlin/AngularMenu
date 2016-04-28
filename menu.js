f91b60b4091853f3024b1bb3fe49c7c25edc9216
Jerry Lin <jeroldlin@gmail.com>
2016-04-28 07:48:54 -0500

    Added Bootstrap and socket.io

--- menu.js
+++ menu.js
@@ -1,19 +1,20 @@
 angular.module('menuApp', [])
-  .controller('MenuListController', function() {
-	
+.controller('MenuListController', function() {
+
 	$(".menuOrder").css("display","none");
-	
-    var menuList = this;
-    menuList.items = [
-      {text:'Caffe Latte', selected:false, price:2.95},
-      {text:'Caffe Mocha', selected:false, price:3.45},
-	  {text:'White Chocolate Mocha', selected:false, price:3.75},
-	  {text:'Caramel Macchiato', selected:false, price:3.75},
-	  {text:'Freshly Brewed Coffee', selected:false, price:1.85}];
-	  
+
+	var menuList = this;
+	menuList.items = [
+		{text:'Caffe Latte', selected:false, price:2.95},
+		{text:'Caffe Mocha', selected:false, price:3.45},
+		{text:'White Chocolate Mocha', selected:false, price:3.75},
+		{text:'Caramel Macchiato', selected:false, price:3.75},
+		{text:'Freshly Brewed Coffee', selected:false, price:1.85}
+	];
+
 	menuList.comments = [];
 
-    menuList.addItem = function(index) {
+	menuList.addItem = function(index) {
 		if (menuList.items[index].selected == true){
 			menuList.items[index].selected = false;
 		}else{
@@ -24,38 +25,38 @@ angular.module('menuApp', [])
 		}else{
 			$(".menuOrder").css("display","");
 		};
-    };	
+	};	
 
-    menuList.addComment = function() {
+	menuList.addComment = function() {
 		if ($(".menuCommentInput").val() == ""){
 			alert ('must input comments!');
 		}else{
 			menuList.comments.push({text:menuList.commentText});
 			menuList.commentText = '';
 		}
-    };
+	};
 
 	menuList.deleteComment = function(index) {
-	  menuList.comments.splice(index, 1);
-    };
- 
-    menuList.totalCost = function() {
+		menuList.comments.splice(index, 1);
+	};
+
+	menuList.totalCost = function() {
 		return totalCost();
-    };
-	
+	};
+
 	function totalCost(){
-      var total = 0;
-      angular.forEach(menuList.items, function(item) {
-		if (item.selected == true) total += item.price;
-      });
-      return total;
+		var total = 0;
+		angular.forEach(menuList.items, function(item) {
+			if (item.selected == true) total += item.price;
+		});
+		return total;
 	}
- 
-    // menuList.archive = function() {
-      // var oldTodos = menuList.items;
-      // menuList.items = [];
-      // angular.forEach(oldTodos, function(item) {
-        // if (!item.selected) menuList.items.push(item);
-      // });
-    // };
-  });
+
+	// menuList.archive = function() {
+	// var oldTodos = menuList.items;
+	// menuList.items = [];
+	// angular.forEach(oldTodos, function(item) {
+	// if (!item.selected) menuList.items.push(item);
+	// });
+	// };
+	});
